64c6e68da218c7b409f10c3e0cfd0c5b5529ca89
adamwdennis <adam.dennis@gmail.com>
2012-11-06 09:53:12 -0400

    fixing textarea class

--- githubPullRequestTemplate.js
+++ githubPullRequestTemplate.js
@@ -1,6 +1,6 @@
 var fieldValueMap = {
   '#pull_request_title' : '#issueId: Brief Description',
-  '.write-content.js-write-bucket': "###### High-level Description - Fix/Feature" +
+  '.js-comment-field.js-size-to-fit': "###### High-level Description - Fix/Feature" +
     "\n\n* " +
     "\n\n###### Expected Usage (Reproduction steps / Steps to use feature)" +
     "\n\n* " +
